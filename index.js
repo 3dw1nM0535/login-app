0af5c35811fe85afa073cc2e600b0a5f29750ccf
Edwin Moses <workockmoses@gmail.com>
2017-09-18 19:43:36 +0300

    imported core modules for App

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exhb = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+
+//Routes configuration
+var routes = require('./Routes/index');
+var users = require('./Routes/users');
+
+//Init App
+var app = express();
