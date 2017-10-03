226f4cc422ae11525c4513e98b8e9f22a9dfef8e
3dw1nM0535 <workockmoses@gmail.com>
2017-10-03 16:54:40 +0300

    used passportjs module for login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../model/model');
+var passport = require('[assport');
+var LocalStrategy = require('passport-local').Strategy;
 
 //Register
 router.get('/register', function (req, res) {
@@ -52,6 +54,26 @@ router.post('/register', function (req, res) {
     res.redirect('/users/login');
   }
 
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+//Login Middleware
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: 'users/login', failureFlash: true }), function (req, res) {
+  res.redirect('/');
 });
 
 //Module export
